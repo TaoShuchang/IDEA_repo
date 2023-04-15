3b5846fc9610c1b442d8971d3ca0b6f755b28536
Shuchang Tao <1012193172@qq.com>
2023-04-15 23:01:21 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,32 @@
-## cora
+# Poisoning attack (MetaAttack) at training time
+# Cora
 python -u main.py --dataset cora --lambda_inv_risks 100 --num_sample 2 --perturb_size 1e-4
 
-## citeseer
+# Citeseer
 python -u main.py --dataset citeseer  --lambda_inv_risks 50 --perturb_size 1e-4
 
-## reddit
+# Reddit
 python -u main.py --dataset reddit --lambda_inv_risks 100 --suffix idea --num_sample 2 --perturb_size 1e-4
 
-## ogbproducts
+# ogbn-products
 python -u main.py --dataset ogbproducts --num_sample 4 
 
-## ogbarxiv
+# ogbn-arxiv
 nohup python -u main.py --dataset ogbarxiv --num_sample 2
+
+
+# Evasion attack  (nettack, PGD, G-NIA, TDGIA) at testing time
+# Cora
+python -u test.py --dataset cora 
+
+# Citeseer
+python -u test.py --dataset citeseer 
+
+# Reddit
+python -u test.py --dataset reddit 
+
+# ogbn-products
+python -u test.py --dataset ogbproducts 
+
+# ogbn-arxiv
+nohup python -u test.py --dataset ogbarxiv
