ea543f035846d2b4bdfd465b9859ac5d87fe1ffd
Shuchang Tao <1012193172@qq.com>
2023-05-31 22:08:33 +0800

    Update

--- run.sh
+++ run.sh
@@ -3,16 +3,16 @@
 nohup python -u main.py --dataset cora --alpha 100  --dom_num 10  --device 1 > log/cora.log 2>&1 &
 
 # Citeseer
-nohup python -u main.py --dataset citeseer  --alpha 100 --dom_num 10 --num_sample 8 > log/citeseer.log 2>&1 &
+nohup python -u main.py --dataset citeseer  --alpha 10 --dom_num 10 --perturb_size 1e-4 --num_sample 8 > log/citeseer.log 2>&1 &
 
 # Reddit
-nohup python -u main.py --dataset reddit --alpha 10 --dom_num 10 --perturb_size 1e-3 --device 2 > log/reddit.log 2>&1 &
+nohup python -u main.py --dataset reddit --alpha 10 --dom_num 10 --device 2 > log/reddit.log 2>&1 &
 
 # ogbn-products
-nohup python -u main.py --dataset ogbproducts --alpha 10 --dom_num 10 --perturb_size 1e-3 --device 3 > log/ogbproducts.log 2>&1 &
+nohup python -u main.py --dataset ogbproducts --alpha 10 --dom_num 10 --device 3 > log/ogbproducts.log 2>&1 &
 
 # ogbn-arxiv
-nohup nohup python -u main.py --dataset ogbarxiv --alpha 10 --dom_num 10 --perturb_size 1e-3 > log/ogbarxiv.log 2>&1 &
+nohup nohup python -u main.py --dataset ogbarxiv --alpha 10 --dom_num 10 > log/ogbarxiv.log 2>&1 &
 
 
 # Evasion attack  (nettack, PGD, TDGIA, and G-NIA) at testing time
@@ -20,7 +20,7 @@ nohup nohup python -u main.py --dataset ogbarxiv --alpha 10 --dom_num 10 --pertu
 nohup python -u test.py --dataset cora > log/eva_cora.log 2>&1 &
 
 # Citeseer
-nohup python -u test.py --suffix a10 --dataset citeseer  > log/eva_citeseer_a10.log 2>&1 &
+nohup python -u test.py --dataset citeseer  > log/eva_citeseer.log 2>&1 &
 
 # Reddit
 nohup python -u test.py --dataset reddit  > log/eva_reddit.log 2>&1 &
