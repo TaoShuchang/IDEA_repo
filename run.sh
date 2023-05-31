14a2cb04a24d8bc35c564ef4346cd6889c8c70d2
Shuchang Tao <1012193172@qq.com>
2023-05-31 20:54:56 +0800

    Update

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 # Poisoning attack (MetaAttack) at training time
 # Cora
-nohup python -u main.py --dataset cora --alpha 100  --dom_num 10 > log/cora.log 2>&1 &
+nohup python -u main.py --dataset cora --alpha 100  --dom_num 10  --device 1 > log/cora.log 2>&1 &
 
 # Citeseer
-nohup python -u main.py --dataset citeseer  --alpha 100 --dom_num 10 --num_sample 8 --device 1 > log/citeseer.log 2>&1 &
+nohup python -u main.py --dataset citeseer  --alpha 100 --dom_num 10 --num_sample 8 > log/citeseer.log 2>&1 &
 
 # Reddit
-nohup python -u main.py --dataset reddit --alpha 10 --dom_num 10 --num_sample 4 --perturb_size 1e-3 --device 2 > log/reddit.log 2>&1 &
+nohup python -u main.py --dataset reddit --alpha 10 --dom_num 10 --perturb_size 1e-3 --device 2 > log/reddit.log 2>&1 &
 
 # ogbn-products
-nohup python -u main.py --dataset ogbproducts --alpha 10 --dom_num 10 --num_sample 4 --perturb_size 1e-3 --device 3 > log/ogbproducts.log 2>&1 &
+nohup python -u main.py --dataset ogbproducts --alpha 10 --dom_num 10 --perturb_size 1e-3 --device 3 > log/ogbproducts.log 2>&1 &
 
 # ogbn-arxiv
 nohup nohup python -u main.py --dataset ogbarxiv --alpha 10 --dom_num 10 --perturb_size 1e-3 > log/ogbarxiv.log 2>&1 &
@@ -29,4 +29,4 @@ nohup python -u test.py --dataset reddit  > log/eva_reddit.log 2>&1 &
 nohup python -u test.py --dataset ogbproducts  > log/eva_ogbproducts.log 2>&1 &
 
 # ogbn-arxiv
-nohup nohup python -u test.py --dataset ogbarxiv  > log/eva_ogbarxiv.log 2>&1 &
+nohup python -u test.py --dataset ogbarxiv  > log/eva_ogbarxiv.log 2>&1 &
